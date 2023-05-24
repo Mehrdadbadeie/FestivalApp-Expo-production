8ee2da676432f053f3bb3f41068787d781aefe1e
rayansabah <92931727+rayansabah@users.noreply.github.com>
2023-05-24 10:27:23 +0200

    first

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import StackMenuRouter from './StackMenuRouter';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.gradient} />
+      <StackMenuRouter />
+      
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#a28877',
   },
-});
+  gradient: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundColor: '#e9424f',
+    opacity: 0.5,
+  },
+})
